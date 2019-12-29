99c885b3d419c2095a342a2bbaea10559cac25dd
Stephen Weatherford (MSFT) <StephenWeatherford@users.noreply.github.com>
2019-12-28 19:42:21 -0700

    Initial scripts.sh

---
+++ scripts.sh
@@ -0,0 +1,2 @@
+alias godors='cd ~/Library/Application\ Support/Steinberg/Dorico\ 3/Script\ Plug-ins/'
+alias newdorm='cp ~/Library/Application\ Support/Steinberg/Dorico\ 3/Script\ Plug-ins/usermacro.lua $1'
