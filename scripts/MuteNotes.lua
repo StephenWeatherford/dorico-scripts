e4bca3049d111a2f21a12731b770fe0f7720bbbd
Stephen Weatherford <stephwe@microsoft.com>
2020-01-18 19:07:34 -0800

    Use magenta for muted notes

--- scripts/MuteNotes.lua
+++ scripts/MuteNotes.lua
@@ -1,4 +1,4 @@
 local app=DoApp.DoApp()
 app:doCommand([[UI.InvokePropertyChangeValue?Type=kEventMuted&Value=true]])
 app:doCommand([[UI.InvokePropertyChangeValue?Type=kEventColour&Value=#ff000000]])
-app:doCommand([[UI.InvokePropertyChangeValue?Type=kEventColour&Value=#ff800080]])
+app:doCommand([[UI.InvokePropertyChangeValue?Type=kEventColour&Value=#ffff80ff]])
